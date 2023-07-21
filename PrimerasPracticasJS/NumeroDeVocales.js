baec161d28288b7d92d3ff0c9360d6b0906d4203
jonathanesquivel <jona.esquivel.cruz@gmail.com>
2023-07-21 14:29:56 -0600

    Se realizaron los cambios en el archivo NumeroDeVocales.js

--- PrimerasPracticasJS/NumeroDeVocales.js
+++ PrimerasPracticasJS/NumeroDeVocales.js
@@ -2,13 +2,11 @@
 
 let text = 'Hola Mundo soy Jona Crack'
 
-text = text.toUpperCase()
-
-let textArray = text.split('')
-
 function countVowels(text) {
+
     let vowels = ['A', 'E', 'I', 'O', 'U']
     let numVowels = 0
+    let textArray = text.toUpperCase().split('')
 
     for (let i = 0; i < textArray.length; i++) {
         if (vowels.includes(textArray[i])) {
