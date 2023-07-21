515ea10258effb5a90f1f8f3202aa47f1bbb8736
jonathanesquivel <jona.esquivel.cruz@gmail.com>
2023-07-21 11:49:47 -0600

    Se realizaron los cambios de buenas practicas

--- ValidarTipoArchivo.js
+++ ValidarTipoArchivo.js
@@ -1,16 +1,17 @@
 /*Validar Tipo de Archivo*/
-    function ValidarImagen(input){
+function ValidarImagen(input){
     let imagen = input.files[0]
+
     let arr = ['image/jpg','image/jpeg','image/png']
 
     if (arr.indexOf(imagen.type) != -1 ){
-    console.log('es valido')
-}
+        console.log('es valido')
+    }
+
     else{
-    input.value = ''
-    console.log('no es valido')
-    alert('No es Valido, Selecciona otro archivo')
-}
+        input.value = ''
+        alert('No es Valido, Selecciona otro archivo')
+    }
 
     console.log(input)
 }
